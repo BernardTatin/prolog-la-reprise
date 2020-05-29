feefc8f62dff24f296b807661f9da141a382e3aa
bernard (pop) tat1 <bernard.tatin@outlook.fr>
2020-05-29 15:46:32 +0200

    simple_calc: more comments

--- src/dcg/simple_calc.pl
+++ src/dcg/simple_calc.pl
@@ -7,10 +7,19 @@
 %%
 %% ===============================
 
+%% swipl needs it
 :- use_module(library(apply)).
 :- use_module(library(porter_stem)).
 
-%% 
+/*
+ grammar definition
+
+ program --> exp op expr.
+ expr --> number
+    | ( expr op expr ).
+ op --> + | - | * | /.
+
+*/
 program(node(O, E1, E2)) --> expr(E1), oper(O), expr(E2).
 expr(node(O, E1, E2)) --> ['('], expr(E1), oper(O), expr(E2), [')'].
 expr(node(number, N)) --> [N], {number(N)}.
@@ -19,6 +28,9 @@ oper(*) --> [*].
 oper(/) --> [/].
 oper(-) --> [-].
 
+/*
+    helper definitions
+*/
 isum(A, B, S) :- S is A + B.
 imul(A, B, S) :- S is A * B.
 isub(A, B, S) :- S is A - B.
@@ -36,12 +48,18 @@ show_out(N) :-
 show_out(X) :-
     format('~w ~46t ~g~12|~n', ['>>', X]).
 
+/*
+    executions
+*/    
+
+%% simple number
 execute(node(number, Out), Out).
+%% expression with an operator
 execute(node(+, E1, E2), Out) :- iexecute(isum, E1, E2, Out).
 execute(node(-, E1, E2), Out) :- iexecute(isub, E1, E2, Out).
 execute(node(*, E1, E2), Out) :- iexecute(imul, E1, E2, Out).
 execute(node(/, E1, E2), Out) :- iexecute(idiv, E1, E2, Out).
-
+%% expression is a string
 execute(StrProgram) :-
     \+ is_list(StrProgram),
     %% see: https://www.swi-prolog.org/pldoc/doc_for?object=section(%27packages/nlp.html%27)
@@ -49,12 +67,15 @@ execute(StrProgram) :-
     %% the grammar definition easier
     tokenize_atom(StrProgram, TokenList),
     execute(TokenList).
-
+%% expression is a list of symbol
 execute(Program) :-
     phrase(program(Tree), Program),
     execute(Tree, Out),
     show_out(Out).
 
+/*
+    tests
+ */
 tst_execute :-
     execute([4, *, '(', 6, /, 3.2, ')']),
     execute([4, *, '(', 6, /, 3, ')']),
